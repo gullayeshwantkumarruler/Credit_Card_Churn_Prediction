8c907875048e0e5629c645d6b6caab151153236f
Yeshwant_Kumar <56618271+gullayeshwantkumarruler@users.noreply.github.com>
2022-03-20 01:21:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ def main():
     Income_Category=col2.selectbox("Income Category",("Less than $40K","$40K - $60K","$80K - $120K","$60K - $80K","abc","$120K +"))
     Card_Category=col1.selectbox("Category of Card",("Blue","Silver","Gold","Platinum"))
     result=""
-    if st.button("Predict"):          Education_Level,Marital_Status,Income_Category,Card_Category
+    if st.button("Predict"):        
         result=predict_churn(Customer_Age,Months_on_book,Total_Relationship_Count,Months_Inactive_12_mon,Contacts_Count_12_mon,Credit_Limit,Total_Revolving_Bal,Avg_Open_To_Buy,Total_Amt_Chng_Q4_Q1,Total_Trans_Amt,Total_Trans_Ct,Total_Ct_Chng_Q4_Q1,Avg_Utilization_Ratio,Gender,Dependent_count,Education_Level,Marital_Status,Income_Category,Card_Category)
     st.success('The output is {}'.format(result))
     if st.button("About"):
