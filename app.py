58801c8d6733d45d0ed1d17fb165871559e606ef
Yeshwant_Kumar <56618271+gullayeshwantkumarruler@users.noreply.github.com>
2022-03-24 01:08:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def predict_churn(Customer_Age,Months_on_book,Total_Relationship_Count,Months_In
     
     prediction=best_model.predict(data2)
     print(prediction)
-    return int(round(prediction[0], 0))
+    return int(round(prediction[0], 0)), prediction
 
 
 
@@ -70,7 +70,7 @@ def main():
     if st.button("Predict"):        
         result=predict_churn(Customer_Age,Months_on_book,Total_Relationship_Count,Months_Inactive_12_mon,Contacts_Count_12_mon,Credit_Limit,Total_Revolving_Bal,Avg_Open_To_Buy,Total_Amt_Chng_Q4_Q1,Total_Trans_Amt,Total_Trans_Ct,Total_Ct_Chng_Q4_Q1,Avg_Utilization_Ratio,Gender,Dependent_count,Education_Level,Marital_Status,Income_Category,Card_Category)
         if result==1: 
-            st.success('The Customer churned')
+            st.success('The Customer churned', prediction)
         else:
             st.success('The Customer did not churn')
             
