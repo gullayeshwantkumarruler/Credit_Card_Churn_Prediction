aae7e012afd071113ed03a8c5af8440a80245275
Yeshwant_Kumar <56618271+gullayeshwantkumarruler@users.noreply.github.com>
2022-03-24 00:34:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from sklearn.impute import SimpleImputer
 import joblib
 import pickle
 
-best_model=joblib.load("pickle_tuned_adb2.pkl")
+best_model=joblib.load("pickle_tuned_adb3.pkl")
 x_train=joblib.load("dataset.pkl")
 
 
