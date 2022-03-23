8690e123a94cafafbba3382feaa94440360f22a9
Yeshwant_Kumar <56618271+gullayeshwantkumarruler@users.noreply.github.com>
2022-03-23 21:31:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ def predict_churn(Customer_Age,Months_on_book,Total_Relationship_Count,Months_In
     l=[Customer_Age,Months_on_book,Total_Relationship_Count,Months_Inactive_12_mon,Contacts_Count_12_mon,Credit_Limit,Total_Revolving_Bal,Avg_Open_To_Buy,Total_Amt_Chng_Q4_Q1,Total_Trans_Amt,Total_Trans_Ct,Total_Ct_Chng_Q4_Q1,Avg_Utilization_Ratio,Gender,Dependent_count,Education_Level,Marital_Status,Income_Category,Card_Category]
     cols=['Customer_Age','Months_on_book','Total_Relationship_Count','Months_Inactive_12_mon','Contacts_Count_12_mon','Credit_Limit','Total_Revolving_Bal','Avg_Open_To_Buy','Total_Amt_Chng_Q4_Q1','Total_Trans_Amt','Total_Trans_Ct','Total_Ct_Chng_Q4_Q1','Avg_Utilization_Ratio','Gender','Dependent_count','Education_Level','Marital_Status','Income_Category','Card_Category']
     data2=pd.DataFrame([l], columns=cols)
-    data2 = pd.get_dummies( data2, columns=data2.select_dtypes(include=["object", "category"]).columns.tolist(), drop_first=True,)
     data2["Income_Category"].replace("abc", np.nan, inplace=True)
+    data2 = pd.get_dummies( data2, columns=data2.select_dtypes(include=["object", "category"]).columns.tolist(), drop_first=True,)
     data2 = data2.reindex(columns = x_train.columns, fill_value=0)
     
     data2 = pd.get_dummies( data2, columns=data2.select_dtypes(include=["object", "category"]).columns.tolist(), drop_first=True,)
