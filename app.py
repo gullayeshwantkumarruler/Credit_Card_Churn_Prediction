31874efcb0ffd085a3db57716faea3e167e4c2a7
Yeshwant_Kumar <56618271+gullayeshwantkumarruler@users.noreply.github.com>
2022-03-24 01:07:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,9 +70,9 @@ def main():
     if st.button("Predict"):        
         result=predict_churn(Customer_Age,Months_on_book,Total_Relationship_Count,Months_Inactive_12_mon,Contacts_Count_12_mon,Credit_Limit,Total_Revolving_Bal,Avg_Open_To_Buy,Total_Amt_Chng_Q4_Q1,Total_Trans_Amt,Total_Trans_Ct,Total_Ct_Chng_Q4_Q1,Avg_Utilization_Ratio,Gender,Dependent_count,Education_Level,Marital_Status,Income_Category,Card_Category)
         if result==1: 
-            st.success('The Customer churned)
+            st.success('The Customer churned')
         else:
-            st.success('The Customer did not churn)
+            st.success('The Customer did not churn')
             
     if st.button("About"):
         st.text("Credit Card Users Churn Prediction")
